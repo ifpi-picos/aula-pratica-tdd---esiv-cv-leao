0f63a34f977f726cde7063cfcb8c7ce28605a055
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2023-04-24 18:53:06 -0300

    Passo 14

--- dollar.java
+++ dollar.java
@@ -8,7 +8,7 @@ class Dollar {
       amount= amount * multiplier;
    }
 
-   Dollar times(int multiplier) {
+   Money times(int multiplier)  {
       return new Dollar(amount * multiplier);
    }
 
@@ -47,14 +47,16 @@ class Franc {
    private int amount;					
    Franc(int amount) {      
       this.amount= amount;
-    }					
-    Franc times(int multiplier)  {      
-       return new Franc(amount * multiplier);					
-    }   
+    }
+
+    Money times(int multiplier)  {
+      return new Franc(amount * multiplier);
+    }
+
     public boolean equals(Object object) {					
        Franc franc = (Franc) object;      
        return amount == franc.amount;					
-     }					
+    }					
 }
 
 class Money  {
