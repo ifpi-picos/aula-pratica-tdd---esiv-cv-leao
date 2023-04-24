c3a8fe3bb10d447ba56110ca5daa532481be292f
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2023-04-24 13:16:23 -0300

    Passo 5

--- dollar.java
+++ dollar.java
@@ -14,8 +14,6 @@ class Dollar {
 
 public void testMultiplication() {
    Dollar five = new Dollar(5);
-   Dollar product = five.times(2);
-   assertEquals(10, product.amount);
-   product = five.times(3);
-   assertEquals(new Dollar(15), product);
+   assertEquals(new Dollar(10), five.times(2));
+   assertEquals(new Dollar(15), five.times(3));
 }
