aa72a2ef0420c78015c919670aab533889e00227
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2023-04-24 12:01:22 -0300

    Passo 2

--- dollar.java
+++ dollar.java
@@ -1,11 +1,15 @@
 class Dollar {
-       int amount = 10;
-       Dollar(int amount) {}			
-       void times(int multiplier) {}
-    }	
+   int amount;
+   Dollar(int amount) {
+      this.amount= amount;
+   }
+   void times(int multiplier) {
+      amount= amount * multiplier;
+   }
+}	
 
-    public void testMultiplication() {
-       Dollar five = new Dollar(5);
-       five.times(2);
-       assertEquals(10, five.amount);
-    }
+public void testMultiplication() {
+    Dollar five = new Dollar(5);
+    five.times(2);
+    assertEquals(10, five.amount);
+}
